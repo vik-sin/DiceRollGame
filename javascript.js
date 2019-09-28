056cda23d185e865e479425a6da55f3b38cb6dcf
Vikas Singhal <46290943+vik-sin@users.noreply.github.com>
2019-09-28 21:31:39 +0530

    Add files via upload
    Timer of 20 seconds is added to the game.

--- javascript.js
+++ javascript.js
@@ -1,38 +1,55 @@
 
 var sum=0;
 
+var t=20;
+
 function score(){
 
-	var num = Math.floor( (Math.random() *6 ) )+1;
-	var source= "dice-" + num + ".png";
+	if(t<2+0){
+		document.getElementById('roll').disable = true;
+		alert("Your time is over.");
+	}
 
-	document.getElementById("pic").src=source;
-	sum=sum+num;
+	else if(t<20 && t>=0){
+
+
+		var num = Math.floor( (Math.random() *6 ) )+1;
+		var source= "dice-" + num + ".png";
+
+		document.getElementById("pic").src=source;
+		sum=sum+num;
    
-	document.getElementById("score").innerHTML = sum;
+		document.getElementById("score").innerHTML = sum;
+
+	}
+
+	else if(t===20){
+
+		var num = Math.floor( (Math.random() *6 ) )+1;
+		var source= "dice-" + num + ".png";
+
+		document.getElementById("pic").src=source;
+		sum=sum+num;
+   
+		document.getElementById("score").innerHTML = sum;
+
+		timer();
+	}
  
 }
 
-// function timer{
+function timer(){
 
-// 	 var now=new Date.getTime();
-// 	 const time= now.getSeconds()+20;
-
-// 	 var dist=time-now;
-
-// 	 document.getElementById("timerValue").innerHTML=dist;
-// // }
-
-// function timer{}
-// var timeleft = 20;
-// var downloadTimer = setInterval(function(){
-//   document.getElementById("timerVakue").innerHTML = timeleft + " seconds";
-//   timeleft -= 1;
-//   if(timeleft <= 0){
-//     clearInterval(downloadTimer);
-//     document.getElementById("text").innerHTML = "Finished"
-//   }
-// }, 1000);
+  setTimeout(function() {timer();}, 1000);
+  if (t<0){
+  	document.getElementById("roll").disabled=true;
+  }
+  else{
+  document.getElementById("timerValue").innerHTML = t;
+   t -= 1;
+}
+ 
+}
 
 
 
